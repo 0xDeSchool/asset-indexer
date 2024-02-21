8132a4cb152ff1b6759f2196e5255797fd4970f2
waite1x <waite1x@outlook.com>
2024-02-21 14:02:17 +0800

    fix

--- project.ts
+++ project.ts
@@ -38,7 +38,7 @@ const project: EthereumProject = {
      * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
      * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
-    endpoint: ["https://polygon-mumbai.blockpi.network/v1/rpc/public"],
+    endpoint: ["https://polygon-mumbai.infura.io/v3/d8e813dfdc3c4d7690fa306835ed47b0"],
   },
   dataSources: [{
     kind: EthereumDatasourceKind.Runtime,
